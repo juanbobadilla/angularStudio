358cce429867c0e5f4fcdd07af1ce64364575c1b
juanPabloBobadilla <jpaz355@gmail.com>
2020-10-04 17:49:02 -0500

    se modifica el componente nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
+import { AboutComponent } from './Components/about/about.component';
 import { BodyComponent } from './Components/body/body.component';
 
 const routes: Routes = [
   {path:'', component:BodyComponent},
+  {path:'about',component:AboutComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './Components/nav/nav.component';
 import { BodyComponent } from './Components/body/body.component';
+import { AboutComponent } from './Components/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    BodyComponent
+    BodyComponent,
+    AboutComponent
     
   ],
   imports: [
