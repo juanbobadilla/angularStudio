0aca01ef7b9a64521cb6e8ce087e5f64ff236e00
juanPabloBobadilla <jpaz355@gmail.com>
2020-10-05 15:03:09 -0500

    se agrega el metodo de editar y cancelar y se deja validada la app

--- src/app/Components/body/body.component.ts
+++ src/app/Components/body/body.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import {dato} from '../models/dato';
+import { dato } from '../models/dato';
 
 @Component({
   selector: 'app-body',
@@ -10,35 +10,71 @@ import {dato} from '../models/dato';
 export class BodyComponent implements OnInit {
 
   //variables
-  users:dato[]=[];
-  
+  users: dato[] = [];
+  formEditActivated: boolean = false;
+  formActivated: boolean = true;
 
-  constructor() { 
-    this.users=[];
+  emailUpdate: string = "";
+  passwordUpdate: string = "";
+  idUpdate: number;
+
+  constructor() {
+    this.users = [];
   }
 
   ngOnInit(): void {
   }
 
   //metodos
-  Add(email:HTMLInputElement ,password:HTMLInputElement){
-    if (email.value =="" || password.value=="") {
+  Add(email: HTMLInputElement, password: HTMLInputElement) {
+    if (email.value == "" || password.value == "") {
       alert("ingrese todos los valores");
-    }else{
+    } else {
       this.users.push(new dato(email.value, password.value, this.users.length));
     }
-    email.value ="";
-    password.value="";
+    email.value = "";
+    password.value = "";
     return false;
   }
 
-  Delete(item:any){
+  Delete(item: any) {
     for (let i = 0; i < this.users.length; i++) {
       if (item == this.users[i].id) {
-        this.users.splice(i,1);
+        this.users.splice(i, 1);
+        this.formEditActivated = false;
+        this.formActivated=true;
       }
     }
     return false;
+  }
+
+  edit(id: any) {
+    this.formEditActivated = true;
+    this.formActivated=false;
+    for (let i = 0; i < this.users.length; i++) {
+      if (id == this.users[i].id) {
+        this.emailUpdate = this.users[i].email;
+        this.passwordUpdate = this.users[i].password;
+        this.idUpdate = id;
+      }
+    }
+    return false;
+  }
+
+  editting() {
+    this.users[this.idUpdate].email = this.emailUpdate;
+    this.users[this.idUpdate].password = this.passwordUpdate;
+    this.emailUpdate = "";
+    this.passwordUpdate = "";
+    this.formEditActivated = false;
+    this.formActivated=true;
+  }
+
+  cancel(){
+    this.formEditActivated = false;
+    this.formActivated=true;
+    this.emailUpdate = "";
+    this.passwordUpdate = "";
   }
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,7 +18,9 @@ import { AboutComponent } from './Components/about/about.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
